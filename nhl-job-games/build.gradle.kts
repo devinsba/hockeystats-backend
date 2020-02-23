114ee0375371835f1724d55d867834536853e458
Brian Devins-Suresh <brian.devinssuresh@datadoghq.com>
2020-02-23 17:17:33 -0500

    blah

--- nhl-job-games/build.gradle.kts
+++ nhl-job-games/build.gradle.kts
@@ -18,7 +18,7 @@ jib {
         image = "docker.pkg.github.com/devinsba/hockeystats-backend/nhl-job-games"
     }
 	container {
-		mainClass = "me.hockeystats.nhl.job.seasons.NhlJobSeasonsApplicationKt"
+		mainClass = "me.hockeystats.nhl.job.seasons.NhlJobGamesApplicationKt"
 	}
 }
 
